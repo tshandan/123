7a09a42fd2d499340efb02f0fddf2033ab679251
tshandan <496509048@qq.com>
2016-07-06 11:55:51 +0800

    book
    在多次尝试次下终于成功，解决了没有建立表的问题，办法是重新做一遍，没有地址那一列的问题，将没有的那一列删除，改名字重新添加

--- 123/ViewController.swift
+++ 123/ViewController.swift
@@ -1,25 +1,58 @@
 //
 //  ViewController.swift
-//  123
+//  book
 //
-//  Created by 珊丹 on 16/7/6.
-//  Copyright © 2016年 珊丹. All rights reserved.
+//  Created by 20141105039 on 16/7/4.
+//  Copyright © 2016年 srf. All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
-
+    var db:SQLiteDB!
+    
+    @IBOutlet weak var name: UITextField!
+    @IBOutlet weak var phone: UITextField!
+    @IBOutlet weak var email: UITextField!
+    @IBOutlet weak var address: UITextField!
+    
+    @IBAction func save(sender: AnyObject) {
+        saveUser()
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        //获取数据库实例
+        db = SQLiteDB.sharedInstance()
+        //如果表还不存在则创建表（其中uid为自增主键）
+        db.execute("create table if not exists user(uid integer primary key,name varchar(20),mobile varchar(20),email varchar(20),adress)")
+        //如果有数据则加载
+      
+        
     }
-
+    
+    
+    //从SQLite加载数据
+   
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
+    //保存数据到SQLite
+    func saveUser() {
+        let name = self.name.text!
+        let phone = self.phone .text!
+        let address = self.address.text!
+        let email = self.email.text!
+        //插入数据库，这里用到了esc字符编码函数，其实是调用bridge.m实现的
+        let sql = "insert into user(name,mobile,email,adress) values('\(name)','\(phone)','\(email)','\(address)')"
+        print("sql: \(sql)")
+        //通过封装的方法执行sql
+        let result = db.execute(sql)
+        print(result)
+    }
 }
 
